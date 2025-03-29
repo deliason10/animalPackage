1f5fd11408148f86c0c7dfd1a34d32a41c28292c
Derek <d.eliason@hotmail.com>
2025-03-28 18:26:05 -0700

    Added the puppy package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/deliason10/animalPackage
 
 go 1.24.1
+
+require github.com/deliason10/puppy v0.0.0-20250329011749-43c8b352c255
--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/deliason10/puppy"
+)
 
 func main(){
 	fmt.Println("Hello again!")
+
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
 }
